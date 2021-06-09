d61cbcd65e7d26a0dcc47d32dad784d3f9cba361
Michal Karol <michal.p.karol@gmail.com>
2021-06-09 14:37:26 +0200

    Adjust

--- cocoDataset.py
+++ cocoDataset.py
@@ -3,22 +3,21 @@ import cv2 as cv
 from pycocotools.coco import COCO
 import os
 
+class CocoDatasetIterator:
+    def __init__(self, dataset):
+        self._dataset = dataset
+        self._index = 0
+
+    def __next__(self):
+        if (self._index < len(self._dataset._coco)):
+            imageid = self._dataset._coco[self._index]
+            image_path = os.path.join(self._dataset._root, self._dataset._coco.loadImgs(imageid)[0]["file_name"])
+            image = cv.imread(image_path)
+            annotations = self._dataset._coco.loadAnns(self._dataset._coco.getAnnIds(imageid))
+            self._index +=1
+            return (image, image_path, annotations)
+        raise StopIteration
 class CocoDataset:
-    class CocoDatasetIterator:
-        def __init__(self, dataset):
-            self._dataset = dataset
-            self._index = 0
-
-        def __next__(self):
-            if (self._index < len(self._dataset._coco)):
-                imageid = self._dataset._coco[self._index]
-                image_path = os.path.join(self._dataset._root, self._dataset._coco.loadImgs(imageid)[0]["file_name"])
-                image = cv.imread(image_path)
-                annotations = self._dataset._coco.loadAnns(self._dataset._coco.getAnnIds(imageid))
-                self._index +=1
-                return (image, image_path, annotations)
-            raise StopIteration
-
     def __init__(self, root: str, annFile: str):
         self._coco = COCO(annFile)
         self._ids = list(sorted(self._coco.imgs.keys()))
