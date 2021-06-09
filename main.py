b00b29d3a97f70f48f2fc75fce84f6cbf23eaeff
Michal Karol <michal.p.karol@gmail.com>
2021-06-09 14:51:07 +0200

    Adjust

--- main.py
+++ main.py
@@ -45,8 +45,8 @@ def buildUNetMask(cocoItem):
 
 def resizeToUNet(image, mask):
     return (
-        cv.resize(image, (572, 572)),
-        np.expand_dims(cv.resize(mask, (388, 388)), axis=2)
+        tf.constant(cv.resize(image, (572, 572))),
+        tf.constant(np.expand_dims(cv.resize(mask, (388, 388)), axis=2))
     )
 
 @tf.function
@@ -74,14 +74,14 @@ def load_image_test(image, mask):
 train_dataset = CocoDataset(root="./data_RGB/train", annFile="./data_RGB/train/train.json")
 train_dataset = [buildUNetMask(item) for item in train_dataset]
 train_dataset = [resizeToUNet(image, mask) for image, mask in train_dataset]
-train_dataset = tf.data.Dataset.from_tensor_slices(train_dataset)
+train_dataset = tf.data.Dataset.from_tensor_slices(([x[0] for x in train_dataset], [x[1] for x in train_dataset]))
 train_dataset = train_dataset.map(load_image_train, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 train_dataset = train_dataset.batch(BATCH_SIZE).cache()
 
 test_dataset = CocoDataset(root="./data_RGB/test", annFile="./data_RGB/test/test.json")
 test_dataset = [buildUNetMask(item) for item in test_dataset]
 test_dataset = [resizeToUNet(image, mask) for image, mask in test_dataset]
-test_dataset = tf.data.Dataset.from_tensor_slices(test_dataset)
+test_dataset = tf.data.Dataset.from_tensor_slices(([x[0] for x in test_dataset], [x[1] for x in test_dataset]))
 test_dataset = test_dataset.map(load_image_test)
 test_dataset = test_dataset.batch(BATCH_SIZE)
 
