097c10a24108092c80b71e8d6d9830df71187072
Michal Karol <michal.p.karol@gmail.com>
2021-06-28 20:17:48 +0200

    Fixing masks

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from cocoDataset import CocoDataset
 from pycocotools import mask as maskUtils
 import numpy as np
 import cv2 as cv
+import matplotlib.pyplot as plt
 
 IMAGE_SIZE = 448
 
@@ -36,17 +37,18 @@ def annToRLE(ann, height, width):
 def buildUNetMask(cocoItem):
     (image, _, annotations) = cocoItem
     
-    annMasks = []
+    annMasks = [[np.zeros((IMAGE_SIZE, IMAGE_SIZE))], [np.zeros((IMAGE_SIZE, IMAGE_SIZE))], [np.zeros((IMAGE_SIZE, IMAGE_SIZE))], [np.zeros((IMAGE_SIZE, IMAGE_SIZE))]]
     for annotation in annotations:
         annMask = maskUtils.decode(annToRLE(annotation, IMAGE_SIZE, IMAGE_SIZE))
-        annMask = annMask * int(annotation["category_id"])
-        annMasks.append(annMask)
-    return image, np.expand_dims(np.maximum.reduce(annMasks), axis=2)
+        classNumber = int(annotation["category_id"])
+        annMasks[classNumber].append(annMask)
+    masks = np.concatenate([np.expand_dims(np.maximum.reduce(annClassMasks), axis=2) for annClassMasks in annMasks], axis=2)
+    return image, masks
 
 def resizeToUNet(image, mask):
     return (
         tf.constant(cv.resize(image, (572, 572))),
-        tf.constant(np.expand_dims(cv.resize(mask, (388, 388)), axis=2))
+        tf.constant(cv.resize(mask, (388, 388)))
     )
 
 @tf.function
@@ -70,36 +72,69 @@ def load_image_test(image, mask):
 
     return image, mask
 
+def main(model_path: str):
+    if not model_path:
+        test_dataset = CocoDataset(root="./data_RGB/test", annFile="./data_RGB/test/test.json")
+        test_dataset = [buildUNetMask(item) for item in test_dataset]
+        test_dataset = [resizeToUNet(image, mask) for image, mask in test_dataset]
+        test_dataset = tf.data.Dataset.from_tensor_slices(([x[0] for x in test_dataset], [x[1] for x in test_dataset]))
+        test_dataset = test_dataset.map(load_image_test)
+        test_dataset = test_dataset.batch(BATCH_SIZE)
 
-train_dataset = CocoDataset(root="./data_RGB/train", annFile="./data_RGB/train/train.json")
-train_dataset = [buildUNetMask(item) for item in train_dataset]
-train_dataset = [resizeToUNet(image, mask) for image, mask in train_dataset]
-train_dataset = tf.data.Dataset.from_tensor_slices(([x[0] for x in train_dataset], [x[1] for x in train_dataset]))
-train_dataset = train_dataset.map(load_image_train, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-train_dataset = train_dataset.batch(BATCH_SIZE)
+        train_dataset = CocoDataset(root="./data_RGB/train", annFile="./data_RGB/train/train.json")
+        train_dataset = [buildUNetMask(item) for item in train_dataset]
+        train_dataset = [resizeToUNet(image, mask) for image, mask in train_dataset]
+        train_dataset = tf.data.Dataset.from_tensor_slices(([x[0] for x in train_dataset], [x[1] for x in train_dataset]))
+        train_dataset = train_dataset.map(load_image_train, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        train_dataset = train_dataset.batch(BATCH_SIZE)
 
-test_dataset = CocoDataset(root="./data_RGB/test", annFile="./data_RGB/test/test.json")
-test_dataset = [buildUNetMask(item) for item in test_dataset]
-test_dataset = [resizeToUNet(image, mask) for image, mask in test_dataset]
-test_dataset = tf.data.Dataset.from_tensor_slices(([x[0] for x in test_dataset], [x[1] for x in test_dataset]))
-test_dataset = test_dataset.map(load_image_test)
-test_dataset = test_dataset.batch(BATCH_SIZE)
+        # Create the model.
+        model = unetModel()
+        # Build the model with the input shape
+        # Image is RGB, so here the input channel is 3.
+        model.build(input_shape=(None, 3, 572, 572))
+        model.summary()
 
+        # Write model saving callback.
+        model_save_callback = tf.keras.callbacks.ModelCheckpoint(
+            '/content/gdrive/My Drive/iranian_models/unet/model_checkpoint', monitor='val_loss', verbose=0, save_best_only=True,
+            save_weights_only=False, mode='auto', save_freq='epoch')
 
-# Create the model.
-model = unetModel()
-# Build the model with the input shape
-# Image is RGB, so here the input channel is 3.
-model.build(input_shape=(None, 3, 572, 572))
-model.summary()
+        model.compile(optimizer='adam',
+                    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                    metrics=['accuracy'])
+        model_history = model.fit(train_dataset, epochs=EPOCHS, validation_data=test_dataset, callbacks=[model_save_callback])
 
-# Write model saving callback.
-model_save_callback = tf.keras.callbacks.ModelCheckpoint(
-    '/content/gdrive/My Drive/iranian_models/unet/model_checkpoint', monitor='val_loss', verbose=0, save_best_only=True,
-    save_weights_only=False, mode='auto', save_freq='epoch')
+    if model_path:
+        test_dataset_raw = CocoDataset(root="./data_RGB/test", annFile="./data_RGB/test/test.json")
+        model = tf.keras.models.load_model(model_path)
+        for item in list(test_dataset_raw)[0:3]:
+            test_dataset = [buildUNetMask(item)]
+            test_dataset = [resizeToUNet(image, mask) for image, mask in test_dataset]
+            test_dataset = tf.data.Dataset.from_tensor_slices(([x[0] for x in test_dataset], [x[1] for x in test_dataset]))
+            test_dataset = test_dataset.map(load_image_test)
+            test_dataset = test_dataset.batch(1)
+            xd = model.predict(test_dataset)[0]
+            plt.figure()
+            plt.imshow(item[0], 'gray', interpolation='none')
+            plt.show()
+            plt.figure()
+            plt.imshow(xd[:, :, 0], 'gray', interpolation='none')
+            plt.show()
+            plt.figure()
+            plt.imshow(xd[:, :, 1], 'gray', interpolation='none')
+            plt.show()
+            plt.figure()
+            plt.imshow(xd[:, :, 2], 'gray', interpolation='none')
+            plt.show()
+            plt.figure()
+            plt.imshow(xd[:, :, 3], 'gray', interpolation='none')
+            plt.show()
 
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-
-model_history = model.fit(train_dataset, epochs=EPOCHS, validation_data=test_dataset, callbacks=[model_save_callback])
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(
+        description=__doc__)
+    parser.add_argument('--model-path', help='model_path')
+    args = parser.parse_args()
+    main(args.model_path)
